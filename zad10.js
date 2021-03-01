aa32ffe43326bd719e4953d8c40f25ffd1f65d90
viktorcho97 <79590632+viktorcho97@users.noreply.github.com>
2021-03-01 14:46:52 +0200

    Update zad10.js

--- zad10.js
+++ zad10.js
@@ -1,11 +1,11 @@
 function coneTask(a, b) {
     let s = Math.sqrt(a * a + b * b);
     let volume = Math.PI * a * a * b / 3;
-    console.log("volume = " + volume);
+    console.log("Обемът е = " + volume);
 
     let area = Math.PI * a * (a + s);
-    console.log("area = " + area);
+    console.log("Площта е = " + area);
 }
  
  
-coneTask(7, 9);
+coneTask(2, 4);
